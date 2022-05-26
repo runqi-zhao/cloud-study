5531282b997cd71c77302c4b9082a91599bec59b
zrsaber <529978097@qq.com>
2022-05-26 11:16:59 +0800

    负载均衡以及rest调用

--- cloud-consumer-order80/src/main/java/com/zrq/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/zrq/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.zrq.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- cloud-consumer-order80/src/main/java/com/zrq/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/zrq/controller/OrderController.java
@@ -3,10 +3,9 @@ package com.zrq.controller;
 import com.zrq.entities.CommonResult;
 import com.zrq.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -26,8 +25,29 @@ public class OrderController {
     }
 
 
+    /**
+     * 返回对象为响应体中数据转换成的对象，基本上可以理解为Json
+     * @param id
+     * @return
+     */
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    /**
+     * 返回对象为ResponseEntity对象，包含了响应中的一些重要信息，比如响应头、响应状态码、响应体等
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444,"操作失败");
+        }
     }
 }
